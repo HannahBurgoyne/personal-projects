1a9b92d3f237d279fd603fae485514f9f7fafb62
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-07-17 11:25:30 +1200

    Plan out project goals

---
+++ flashcards-app/index.js
@@ -0,0 +1,14 @@
+// Sprint goals:
+//
+// Flashcards shown in random order to user
+// On user click, flip flashcard around (animate module?)
+// Pre-determined set of flashcard topics (start with 1)
+// Progress bar along bottom of screen
+// User can tick yes or no on each card to rate their progress
+// Score shown at end to user
+
+// Stretch/Bootcamp goals:
+//
+// User can make their own custom cards
+// Collections of cards can be stored in databases and selected
+// Algorithm which shows user their lower rated cards more often
