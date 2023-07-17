4138d67796d39dd52d9e2d41374a631705b1c62e
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-07-17 14:44:22 +1200

    Add data for two decks of flashcards

--- flashcards-app/index.js
+++ flashcards-app/index.js
@@ -1,13 +1,32 @@
-// Sprint goals:
+// ---- Sprint goals ---- //
 //
+
+// Starting topics //
+let frenchVocab = [
+  { q: 'dog', a: 'un chien' },
+  { q: 'cat', a: 'un chat' },
+  { q: 'rabbit', a: 'un lapin' },
+  { q: 'mouse', a: 'une souris' },
+  { q: 'horse', a: 'un cheval' },
+]
+
+let dogTricks = [
+  { q: 'paw', a: 'dog offers each paw to owner' },
+  { q: 'spin', a: 'dog spins clockwise and counter-clockwise' },
+  { q: 'rollover', a: 'dog rolls over both ways' },
+  { q: 'shake', a: 'dog offers each paw and lets each be shaken' },
+  { q: 'weave', a: "dog weaves through owner's legs" },
+]
+
 // Flashcards shown in random order to user
+// Do images or text content
 // On user click, flip flashcard around (animate module?)
 // Pre-determined set of flashcard topics (start with 1)
 // Progress bar along bottom of screen
 // User can tick yes or no on each card to rate their progress
 // Score shown at end to user
 
-// Stretch/Bootcamp goals:
+// ---- Stretch/Bootcamp goals ---- //
 //
 // User can make their own custom cards
 // Collections of cards can be stored in databases and selected
