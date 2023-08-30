7121a244e950b5996369e13793914187df4262d7
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-08-31 11:55:29 +1200

    write api routes

---
+++ client/apiClient.tsx
@@ -0,0 +1,18 @@
+import request from 'superagent'
+import { AllDecks, Deck } from '../models/models'
+
+const baseUrl = '/api/v1/flashcardsapp/'
+
+export async function fetchAllDecks() {
+  const data = await request.get(baseUrl)
+  return data.body as AllDecks[]
+}
+
+export async function fetchDeck(deckId: number) {
+  const data = await request.get(`${baseUrl}${deckId}`)
+  return data.body as Deck
+}
+
+export async function addNewDeck(deckId: number, deckData: Deck) {
+  await request.post(`${baseUrl}${deckId}`).send(deckData)
+}
