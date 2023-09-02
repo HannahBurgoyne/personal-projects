97aa8240f9cf3b5e1e0244fab8860fbd32e8d026
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-09-02 16:44:52 +1200

    add footer

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -1,15 +1,18 @@
 import { Outlet } from 'react-router-dom'
+import Footer from './Footer'
 // This is the layout everything sits upon
 // Outlet goes in here
 
 function Layout() {
   return (
+    <>
     <main>
       <div id="parent-container">
       <Outlet />
-     
       </div>
     </main>
+    <Footer/>
+    </>
   )
 }
 
