b2c7746fe6bbb52bc89931037b67008559751a78
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-09-02 16:42:03 +1200

    structure html on pages

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,8 +1,17 @@
 // Home page, root route
 // Sits inside layout
 
+import { Link } from "react-router-dom"
+
 function Home() {
-  return <p>This is the homepage</p>
+  return (
+    <>
+  <main>
+  <h1>Welcome to flashcards</h1>
+  <Link to="/deck-library"><button>Enter deck library</button></Link>
+  </main>
+  </>
+  )
 }
 
 export default Home
--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -5,28 +5,10 @@ import { Outlet } from 'react-router-dom'
 function Layout() {
   return (
     <main>
-      <p>This is the layout</p>
-      <h1>Flashcards</h1>
       <div id="parent-container">
-        <div className="column">
-          <div className="flashcard-design">
-            <a href="#">Topic 1</a>
-          </div>
-        </div>
-
-        <div className="column">
-          <div className="flashcard-design">
-            <a href="#">Topic 2</a>
-          </div>
-        </div>
-
-        <div className="column">
-          <div className="flashcard-design">
-            <a href="#">Topic 3</a>
-          </div>
-        </div>
-      </div>
       <Outlet />
+     
+      </div>
     </main>
   )
 }
