c7958aa26424a6ca07b0b9ff6b1aae25f2cece9a
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-09-04 10:12:16 +1200

    create form to add new deck

--- client/components/AddNewDeck.tsx
+++ client/components/AddNewDeck.tsx
@@ -2,7 +2,97 @@
 // Sits inside the layout, linked from deck library - or nested inside deck library as a toggle component
 
 function AddNewDeck() {
-  return <p>This is where you can add a new deck</p>
+  return (
+    <>
+      <div className="addformcontainer">
+        <h1>Add new deck</h1>
+        <div className="add-form">
+          <form>
+            <label htmlFor="deck">Give your deck a name and author</label>
+            <input
+              className="new deck"
+              placeholder="deck name"
+              type="text"
+              name="deckName"
+            ></input>
+            <input
+              className="new deck"
+              placeholder="deck author"
+              type="text"
+              name="deckAuthor"
+            ></input>
+            <h4>Enter your flashcards</h4>
+            <label htmlFor="flashcards">Card 1</label>
+            <input
+              className="new deck"
+              placeholder="question "
+              type="text"
+              name="flashcard"
+            ></input>
+            <input
+              className="new deck"
+              placeholder="answer "
+              type="text"
+              name="flashcard"
+            ></input>
+            <label htmlFor="flashcards">Card 2</label>
+            <input
+              className="new deck"
+              placeholder="question"
+              type="text"
+              name="flashcard"
+            ></input>
+            <input
+              className="new deck"
+              placeholder="answer"
+              type="text"
+              name="flashcard"
+            ></input>
+            <label htmlFor="flashcards">Card 3</label>
+            <input
+              className="new deck"
+              placeholder="question"
+              type="text"
+              name="flashcard"
+            ></input>
+            <input
+              className="new deck"
+              placeholder="answer"
+              type="text"
+              name="flashcard"
+            ></input>
+            <label htmlFor="flashcards">Card 4</label>
+            <input
+              className="new deck"
+              placeholder="question"
+              type="text"
+              name="flashcard"
+            ></input>
+            <input
+              className="new deck"
+              placeholder="answer"
+              type="text"
+              name="flashcard"
+            ></input>
+            <label htmlFor="flashcards">Card 5</label>
+            <input
+              className="new deck"
+              placeholder="question"
+              type="text"
+              name="flashcard"
+            ></input>
+            <input
+              className="new deck"
+              placeholder="answer"
+              type="text"
+              name="flashcard"
+            ></input>
+            <button>Submit entire deck</button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default AddNewDeck
--- client/components/DeckLibrary.tsx
+++ client/components/DeckLibrary.tsx
@@ -2,12 +2,14 @@ import { useQuery } from '@tanstack/react-query'
 import { fetchAllDecks } from '../apiClient'
 import Flashcards from './Flashcards'
 import { useState } from 'react'
+import AddNewDeck from './AddNewDeck'
 // This is where the deck library lives
 // Sits inside layout
 
 function DeckLibrary() {
   const { data } = useQuery(['deck'], fetchAllDecks)
   const [useFlashcards, setUseFlashcards] = useState(false)
+  const [showAdd, setShowAdd] = useState(false)
 
   // have an onclick function to show flashcards for the deck you click on
 
@@ -15,39 +17,59 @@ function DeckLibrary() {
     setUseFlashcards(!false)
   }
 
-  return (
-    <section>
-      <div>
-        {data &&
-          data.map((deck) => (
-            <>
-              <div className="library-container">
-                <div key={`flashcard ${deck.id}`} className="deck-container">
-                  <div className="deck-content">
-                    <h2 className="deck-title" key={`name ${deck.id}`}>
-                      {deck.deck_name}
-                    </h2>
-                    <h4 className="deck-author" key={`author ${deck.id}`}>
-                      {deck.author}
-                    </h4>
-                    <button className="deck-button" onClick={showFlashcards}>
-                      Use deck
-                    </button>
-                  </div>
-                </div>
+  function showAddForm() {
+    setShowAdd(!false)
+  }
 
-                {useFlashcards ? (
-                  <>
-                    <Flashcards id={deck.id} />
-                  </>
-                ) : (
-                  ''
-                )}
+  return (
+    <div>
+      {data &&
+        data.map((deck) => (
+          <>
+            <div className="library-container">
+              <div key={`flashcard ${deck.id}`} className="deck-container">
+                <div className="deck-content">
+                  <h2 className="deck-title" key={`name ${deck.id}`}>
+                    {deck.deck_name}
+                  </h2>
+                  <h4 className="deck-author" key={`author ${deck.id}`}>
+                    {deck.author}
+                  </h4>
+                  <button className="deck-button" onClick={showFlashcards}>
+                    Use deck
+                  </button>
+                </div>
               </div>
-            </>
-          ))}
-      </div>
-    </section>
+
+              <div>
+                <button onClick={showAddForm} className="add-deck-btn">
+                  Add new deck
+                </button>
+              </div>
+            </div>
+
+            {useFlashcards ? (
+              <>
+                <section className="use-flashcards-container">
+                  <Flashcards id={deck.id} />
+                </section>
+              </>
+            ) : (
+              ''
+            )}
+
+            {showAdd ? (
+              <>
+                <section className="add-deck-container">
+                  <AddNewDeck />
+                </section>
+              </>
+            ) : (
+              ''
+            )}
+          </>
+        ))}
+    </div>
   )
 }
 
