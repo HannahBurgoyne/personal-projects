212876a0a3305d699dfa4b39cb8672ac0f11f1d5
Hannah Burgoyne <111092622+HannahBurgoyne@users.noreply.github.com>
2023-09-04 16:19:58 +1200

    resolve errors in rendering components

--- client/components/DeckLibrary.tsx
+++ client/components/DeckLibrary.tsx
@@ -8,65 +8,57 @@ import AddNewDeck from './AddNewDeck'
 
 function DeckLibrary() {
   const { data } = useQuery(['deck'], fetchAllDecks)
-  const [useFlashcards, setUseFlashcards] = useState(false)
+  // const [useFlashcards, setUseFlashcards] = useState(false)
   const [showAdd, setShowAdd] = useState(false)
+  const [selectedDeckId, setSelectedDeckId] = useState(null)
 
-
-  function showFlashcards() {
-    setUseFlashcards(!false)
+  function showFlashcards(deckId) {
+    setSelectedDeckId(deckId)
   }
 
   function showAddForm() {
-    setShowAdd(!false)
+    setShowAdd(!showAdd)
   }
 
   return (
     <div className="parent-deck-library">
-      {data &&
-        data.map((deck) => (
-          <>
-            <div className="library-container">
-              <div key={`flashcard ${deck.id}`} className="deck-container">
-                <div className="deck-content">
-                  <h2 className="deck-title" key={`name ${deck.id}`}>
-                    {deck.deck_name}
-                  </h2>
-                  <h4 className="deck-author" key={`author ${deck.id}`}>
-                    {deck.author}
-                  </h4>
-                  <button className="deck-button" onClick={showFlashcards}>
-                    Use deck
-                  </button>
+      {selectedDeckId ? (
+        <section className="use-flashcards-container">
+          <Flashcards id={selectedDeckId} />
+        </section>
+      ) : showAdd ? (
+        <section className="add-deck-container">
+          <AddNewDeck total={data.length} />
+        </section>
+      ) : (
+        <>
+          <button onClick={showAddForm} className="add-deck-btn">
+            Add new deck
+          </button>
+
+          {data &&
+            data.map((deck) => (
+              <div className="library-container">
+                <div key={`deck ${deck.id}`} className="deck-container">
+                  <div className="deck-content">
+                    <h2 className="deck-title" key={`name ${deck.id}`}>
+                      {deck.deck_name}
+                    </h2>
+                    <h4 className="deck-author" key={`author ${deck.id}`}>
+                      {deck.author}
+                    </h4>
+                    <button
+                      className="deck-button"
+                      onClick={() => showFlashcards(deck.id)}
+                    >
+                      Use deck
+                    </button>
+                  </div>
                 </div>
               </div>
-
-              <div></div>
-            </div>
-
-            {useFlashcards ? (
-              <>
-                <section className="use-flashcards-container">
-                  <Flashcards id={deck.id} />
-                </section>
-              </>
-            ) : (
-              ''
-            )}
-
-            {showAdd ? (
-              <>
-                <section className="add-deck-container">
-                  <AddNewDeck total={data.length} />
-                </section>
-              </>
-            ) : (
-              ''
-            )}
-          </>
-        ))}
-      <button onClick={showAddForm} className="add-deck-btn">
-        Add new deck
-      </button>
+            ))}
+        </>
+      )}
     </div>
   )
 }
