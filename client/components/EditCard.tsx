6ecd9630097710f27643f9fa55a77d0a714539e6
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-10-12 12:01:38 +1300

    add function to edit card

--- client/components/EditCard.tsx
+++ client/components/EditCard.tsx
@@ -2,48 +2,60 @@
 // Shows you all the flashcards in your deck, and the option to edit or delete each one
 
 import { useState } from 'react'
-import { FlashcardData } from '../../models/models'
+import { FlashcardData, NewFlashcard } from '../../models/models'
+import { useQueryClient, useMutation } from '@tanstack/react-query'
+import { updateFlashcard } from '../apiClient'
 
 interface Props {
   card: FlashcardData
 }
 
 function EditCard(props: Props) {
-  const [isFlipped, setIsFlipped] = useState(false)
-  const [isEditing, setIsEditing] = useState(false)
+  const queryClient = useQueryClient()
 
-  function flipCard() {
-    setIsFlipped(!isFlipped)
+  const updateMutation = useMutation({
+    mutationFn: (flashcardData: FlashcardData) =>
+      updateFlashcard(flashcardData),
+    onSuccess: () => {
+      queryClient.invalidateQueries(['flashcards'])
+    },
+  })
+
+  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const target = e.currentTarget
+    const form = new FormData(target)
+
+    const updatedQuestion = form.get('flashcard-q')?.valueOf() as string
+    const updatedAnswer = form.get('flashcard-a')?.valueOf() as string
+
+    const updatedCard = {
+      flashcardId: props.card.flashcardId,
+      deckId: props.card.deckId,
+      number: props.card.number,
+      question: updatedQuestion,
+      answer: updatedAnswer,
+    }
+
+    updateMutation.mutate(updatedCard)
   }
-
-  function editCard() {
-    setIsEditing(!isEditing)
-  }
-
-  function handleFormSubmit() {}
 
   return (
     <div className="edit-flashcards-container">
-      <div className="deck-container" onClick={flipCard}>
+      <div className="deck-container">
         <form onSubmit={handleFormSubmit} className="edit-card-form">
           <input
             type="text"
-            name="flashcard"
+            name="flashcard-q"
             placeholder={props.card.question}
           />
-          <input type="text" name="flashcard" placeholder={props.card.answer} />
+          <input
+            type="text"
+            name="flashcard-a"
+            placeholder={props.card.answer}
+          />
           <button>Save</button>
         </form>
-
-        {/* 
-          {isFlipped ? (
-            <p className="card-text animate_animated animate__flipInY">
-              {props.card.answer}
-            </p>
-          ) : (
-            <p className="card-text">{props.card.question}</p>
-          )}
-          {isEditing ? <p>Edit mode</p> : <p>not edit mode</p>} */}
       </div>
     </div>
   )
