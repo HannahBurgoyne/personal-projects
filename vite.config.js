db90e2bc222e612e50fdef6ff1023a38729c2d2a
Hannah <hannah.burgoyne@gmail.com>
2023-10-16 17:10:18 +1300

    change local address

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': `http://localhost:${process.env.PORT || 3000}`,
+      '/api': `http://0.0.0.0:${process.env.PORT || 3000}`,
     },
   },
 })
