1c69c067eb98ae5f63fea7caed99df08a327b260
Hannah <hannah.burgoyne@gmail.com>
2023-10-16 17:31:10 +1300

    move dotenv

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,10 @@
 import * as Path from 'node:path'
 import * as URL from 'node:url'
-import dotenv from 'dotenv'
 
 import server from './server'
 
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
-dotenv.config({ path: Path.join(__dirname, '../.env') })
 
 const PORT = process.env.PORT || 3000
 
--- server/server.ts
+++ server/server.ts
@@ -2,7 +2,7 @@ import * as Path from 'node:path'
 import * as URL from 'node:url'
 import express from 'express'
 import routes from './routes/routes'
-
+import dotenv from 'dotenv'
 const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
@@ -13,6 +13,8 @@ server.use(express.static(Path.join(__dirname, 'public')))
 server.use('/api/v1/flashcardsapp', routes)
 
 if (process.env.NODE_ENV === 'production') {
+  dotenv.config()
+
   server.use(express.static(Path.resolve('public')))
   server.use('/assets', express.static(Path.resolve('./dist/assets')))
   server.get('*', (req, res) => {
