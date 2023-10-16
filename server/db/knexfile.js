762147dc8a4e0570705063e4d519d4fd84ba7165
Hannah <hannah.burgoyne@gmail.com>
2023-10-16 17:55:12 +1300

    change path to dotenv

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -6,7 +6,7 @@ const __filename = URL.fileURLToPath(import.meta.url)
 const __dirname = Path.dirname(__filename)
 
 if (process.env.NODE_ENV === 'production') {
-  dotenv.config({ path: Path.join(__dirname, '.env') })
+  dotenv.config()
 } else {
   dotenv.config({ path: Path.join(__dirname, '../../.env') })
 }
