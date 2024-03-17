97d9085b8b5455b19a5c932ffcd5bfa732897ed4
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-03-18 11:09:01 +1300

    refactor

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,4 @@
-import * as Path from 'node:path'
-import * as URL from 'node:url'
-
 import server from './server'
-
-const __filename = URL.fileURLToPath(import.meta.url)
-const __dirname = Path.dirname(__filename)
 
 const PORT = process.env.PORT || 3000
 
